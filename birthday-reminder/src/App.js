49ce9b7de73ccbeb07c9dd1fac72ff159c10fa4f
ItotiaHarrison <harrison.itotia@student.moringaschool.com>
2022-10-30 23:35:48 +0300

    modify components

--- birthday-reminder/src/App.js
+++ birthday-reminder/src/App.js
@@ -1,10 +1,14 @@
-
+import React from 'react';
 import './App.css';
+import Data from './Data';
+import List from './List';
 
 function App() {
   return (
     <div className="App">
-   
+      <h1>My Contacts</h1>
+   <Data />
+   <List />
     </div>
   );
 }
